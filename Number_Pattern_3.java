784c419ac3e2f05d2a827a69073b3ee3b2069c55
Ratnadeep Umak <123054926+RatnadeepUmak@users.noreply.github.com>
2023-01-24 05:30:05 -0800

    added Number_Pattern_3.java

---
+++ Number_Pattern_3.java
@@ -0,0 +1,35 @@
+package codes;
+import java.util.Scanner;
+public class Number_Pattern_3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int i = 1;
+		
+		while(i <= n) {
+			int j = 1;
+			while(j <= n- i) {
+				System.out.print("  ");
+				j++;
+			}
+			j = 1;
+			while(j <= i) {
+				System.out.print(j+" ");
+				j++;
+			}
+			j = 1;
+			while(j < i) {
+				System.out.print(j+" ");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+		
+		
+
+	}
+
+}
