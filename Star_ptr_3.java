d360b1fe97e1ae32ad60f45313499a0373d0ceb9
Ratnadeep Umak <123054926+RatnadeepUmak@users.noreply.github.com>
2023-01-24 04:54:46 -0800

    added Star_ptr_3.java

---
+++ Star_ptr_3.java
@@ -0,0 +1,27 @@
+package codes;
+import java.util.Scanner;
+public class Star_ptr_3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int i = 1;
+		
+		while(i <= n) {
+			int j = 1;
+			while(j <= n - i) {
+				System.out.print(" ");
+				j++;
+			}
+			 j = 1;
+			while(j <= i) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
